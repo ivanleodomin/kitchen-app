7272173a374b8dd9abc946a631eb8f4ed467abcb
ivanleodomin <ivanleodomin00@gmail.com>
2023-05-25 19:45:15 -0300

    clean interface orderRespoitory

--- src/Domain/repositories/orderRepository.ts
+++ src/Domain/repositories/orderRepository.ts
@@ -6,7 +6,7 @@ export type OrderPage = Page<Order>
 
 export default interface OrderRepository {
 	register(recipe: Recipe): Promise<Order>;
-	getAll(filter?: any, page?: number, skip?: number, limit?: number): Promise<OrderPage>;
+	getAll(filter?: any, page?: number, limit?: number): Promise<OrderPage>;
 	getById(id: string): Promise<Order | null>
 	update(orderId: string, update: any): Promise<Order | null>;
 }
